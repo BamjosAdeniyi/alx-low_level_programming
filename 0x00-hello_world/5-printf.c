e841d688cf2a06d6c19eec0b2ee8712febdbdf8c
Oluwabamise Joseph Adeniyi <bamisejoseph85@gmail.com>
2023-06-15 06:49:37 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0 (Program is succesful)
+ */
+int main(void)
+{ 	
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+       	return (0);
+}
