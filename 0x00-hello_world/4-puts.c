7b8f81da1b7c5a21cef5cb5014cc8683fd6127c2
Oluwabamise Joseph Adeniyi <bamisejoseph85@gmail.com>
2023-06-15 06:44:51 -0700

    4-puts.c update again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - entry point of the program
  *
  * Return: Always 0 (Program is succesful)
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
