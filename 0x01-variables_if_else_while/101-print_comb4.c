438bea23b7a5ed3300601b46e80f67eaf1364de4
Oluwabamise Joseph Adeniyi <bamisejoseph85@gmail.com>
2023-06-16 07:50:09 -0700

    101-print_comb4.c update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (a = 50; a < 58; a++)
+			{
+				if (a > b && b > c)
+				{
+					putchar(c);
+					putchar(b);
+					putchar(a);
+					if (c != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
