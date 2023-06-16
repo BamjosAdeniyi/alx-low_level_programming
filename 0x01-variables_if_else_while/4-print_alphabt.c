61973624182f0fc99a26928dd32563bde1ac52e7
Oluwabamise Joseph Adeniyi <bamisejoseph85@gmail.com>
2023-06-16 04:45:29 -0700

    4-print_alphabt.c update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpaha == e || alpha == q)
+		{
+			continue;
+		}
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
