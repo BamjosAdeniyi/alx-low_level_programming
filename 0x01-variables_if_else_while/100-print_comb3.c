aa1e3d5ad01643b362e2f5090a1a973b1ad4970d
Oluwabamise Joseph Adeniyi <bamisejoseph85@gmail.com>
2023-06-16 07:38:33 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (b = 48; b <= 56; b++)
+	{
+		for (a = 49; a <= 57; a++)
+		{
+			if (a > b)
+			{
+				putchar(b);
+				putchar(a);
+				if (b != 56 || a != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
