9d9bb561e5ccde2846001e1fc29c43fb88a855bd
Oluwabamise Joseph Adeniyi <bamisejoseph85@gmail.com>
2023-06-21 03:03:01 -0700

    1-main.c final update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-
 	int i;
 
 	printf("Infinite loop incoming :(\n");
@@ -19,8 +18,7 @@ int main(void)
 	 *	putchar(i);
 	 * }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
+	
 	return (0);
-
 }
