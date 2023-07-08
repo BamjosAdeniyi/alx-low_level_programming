68aaac4c00d254b66877642364977e3b7a39feab
Oluwabamise Joseph Adeniyi <bamisejoseph85@gmail.com>
2023-07-08 12:03:03 -0700

    lastly

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,13 +5,13 @@
  * infinite_add - adds two numbers
  * @n1: number one.
  * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
+ * @r: store the result.
+ * @size_r: size:
  * Return: the pointer to dest.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int i, j, k, a, b, c;
 
 	for (i = 0; n1[i]; i++)
 		;
@@ -19,30 +19,30 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		;
 	if (i > size_r || j > size_r)
 		return (0);
-	m = 0;
+	b = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		n = m;
+		c = b;
 		if (i >= 0)
-			n += n1[i] - '0';
+			c += n1[i] - '0';
 		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
+			c += n2[j] - '0';
+		if (i < 0 && j < 0 && c == 0)
 		{
 			break;
 		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+		b = c / 10;
+		r[k] = c % 10 + '0';
 	}
 	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
+	if (i >= 0 || j >= 0 || b)
 		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
+	for (k -= 1, a = 0; a < k; k--, a++)
 	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		b = r[k];
+		r[k] = r[a];
+		r[a] = b;
 	}
 	return (r);
 
-
+}
